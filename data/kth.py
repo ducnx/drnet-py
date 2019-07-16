025dca7761b018b51eea93879ba1cd952a4ad0a9
Duc Nguyen <ducngx95@gmail.com>
2019-07-16 15:41:20 +1000

    Remove load_lua

--- data/kth.py
+++ data/kth.py
@@ -4,15 +4,16 @@ import numpy as np
 import socket
 import torch
 from scipy import misc
-from torch.utils.serialization import load_lua
+import torchfile
+
 
 class KTH(object):
 
-    def __init__(self, train, data_root, seq_len = 20, image_size=64, data_type='drnet'):
+    def __init__(self, train, data_root, seq_len=20, image_size=64, data_type='drnet'):
         self.data_root = '%s/KTH/processed/' % data_root
         self.seq_len = seq_len
         self.data_type = data_type
-        self.image_size = image_size 
+        self.image_size = image_size
         self.classes = ['boxing', 'handclapping', 'handwaving', 'jogging', 'running', 'walking']
 
         self.dirs = os.listdir(self.data_root)
@@ -25,16 +26,15 @@ class KTH(object):
             self.persons = list(range(21, 26))
             data_type = 'test'
 
-        self.data= {}
-        for c in self.classes:
-            self.data[c] = load_lua('%s/%s/%s_meta%dx%d.t7' % (self.data_root, c, data_type, image_size, image_size))
-     
+        self.data = {}
+        for c in self.classes:            
+            self.data[c] = torchfile.load(f'{self.data_root}/{c}/{data_type}_meta{image_size}x{image_size}.t7')
 
         self.seed_set = False
 
     def get_sequence(self):
         t = self.seq_len
-        while True: # skip seqeunces that are too short
+        while True:  # skip seqeunces that are too short
             c_idx = np.random.randint(len(self.classes))
             c = self.classes[c_idx]
             vid_idx = np.random.randint(len(self.data[c]))
@@ -44,9 +44,8 @@ class KTH(object):
                 break
         dname = '%s/%s/%s' % (self.data_root, c, vid['vid'])
         st = random.randint(0, len(vid['files'][seq_idx])-t)
-           
 
-        seq = [] 
+        seq = []
         for i in range(st, st+t):
             fname = '%s/%s' % (dname, vid['files'][seq_idx][i])
             im = misc.imread(fname)/255.
@@ -54,7 +53,7 @@ class KTH(object):
         return np.array(seq)
 
     # to speed up training of drnet, don't get a whole sequence when we only need 4 frames
-    # x_c1, x_c2, x_p1, x_p2 
+    # x_c1, x_c2, x_p1, x_p2
     def get_drnet_data(self):
         c_idx = np.random.randint(len(self.classes))
         c = self.classes[c_idx]
@@ -63,8 +62,8 @@ class KTH(object):
         seq_idx = np.random.randint(len(vid['files']))
         dname = '%s/%s/%s' % (self.data_root, c, vid['vid'])
         seq_len = len(vid['files'][seq_idx])
-           
-        seq = [] 
+
+        seq = []
         for i in range(4):
             t = np.random.randint(seq_len)
             fname = '%s/%s' % (dname, vid['files'][seq_idx][t])
@@ -77,7 +76,7 @@ class KTH(object):
             self.seed_set = True
             random.seed(index)
             np.random.seed(index)
-            #torch.manual_seed(index)
+            # torch.manual_seed(index)
         if not self.train or self.data_type == 'sequence':
             return torch.from_numpy(self.get_sequence())
         elif self.data_type == 'drnet':
@@ -86,5 +85,4 @@ class KTH(object):
             raise ValueError('Unknown data type: %d. Valid type: drnet | sequence.' % self.data_type)
 
     def __len__(self):
-        return len(self.dirs)*36*5 # arbitrary
-
+        return len(self.dirs)*36*5  # arbitrary
