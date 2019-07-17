e8ef662f99f2a64a9d51f7cc0e8d7d0745968186
Duc Nguyen <ducngx95@gmail.com>
2019-07-17 16:32:44 +1000

    Update scripts

--- scripts/train_drnet_suncg.sh
+++ scripts/train_drnet_suncg.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python train_drnet.py
+CUDA_VISIBLE_DEVICES=0 python train_drnet.py --batch_size 100 \
+                                             --data_root ./data/ \
+                                             --niter 10 \
+                                             --epoch_size 10 \
+                                             --dataset moving_mnist \
+                                             --device cpu
